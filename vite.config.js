570ed6f37121dd3352c24fd36d92554d90e30cad
ForteScarlet <ForteScarlet@163.com>
2021-07-28 22:44:41 +0800

    update and test for Github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicDir: 'simpler-robot-exhibition'
+  base: 'simpler-robot-exhibition'
 })
